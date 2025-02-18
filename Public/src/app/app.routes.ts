c87aa3c676bf8592a16246aec9d834a76e749056
Kewkew00 <78853822+Kewkew00@users.noreply.github.com>
2025-02-18 11:49:26 +0100

    (Statikus) Profil oldal elkészítése

--- Public/src/app/app.routes.ts
+++ Public/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { LandingpageComponent } from './components/landingpage/landingpage.compo
 import { StatsComponent } from './components/stats/stats.component';
 import { LoginComponent } from './components/login/login.component';
 import { UserAuthGuard } from './guards/user-auth.guard';
+import { ProfileComponent } from './components/profile/profile.component';
 
 export const routes: Routes = [
 
@@ -17,6 +18,10 @@ export const routes: Routes = [
 
     {
         path: 'landingpage', component: LandingpageComponent
+    },
+
+    {
+        path: 'profile', component: ProfileComponent
     },
 
     {
