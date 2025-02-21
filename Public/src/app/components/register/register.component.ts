e329affb50bbbe61324502e960f564c2e5b93c4c
Kewkew00 <78853822+Kewkew00@users.noreply.github.com>
2025-02-21 10:32:42 +0100

    stepper panel alkalmazása

--- Public/src/app/components/register/register.component.ts
+++ Public/src/app/components/register/register.component.ts
@@ -5,11 +5,12 @@ import { FloatLabelModule } from 'primeng/floatlabel';
 import { PasswordModule } from 'primeng/password';
 import { ButtonModule } from 'primeng/button';
 import { RadioButtonModule } from 'primeng/radiobutton';  
+import { StepperModule } from 'primeng/stepper';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [InputTextModule, FormsModule, FloatLabelModule, PasswordModule, ButtonModule , RadioButtonModule],
+  imports: [InputTextModule, FormsModule, FloatLabelModule, PasswordModule, ButtonModule , RadioButtonModule, StepperModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
