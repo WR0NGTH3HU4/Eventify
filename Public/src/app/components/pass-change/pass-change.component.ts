be3adc3616ff349da37fb1661aa4bc80e3dea60e
Kulimák Máté <kulimakmate@turr.hu>
2025-02-21 10:27:02 +0100

    Jelszó változtatás component elkészítése

--- Public/src/app/components/pass-change/pass-change.component.ts
+++ Public/src/app/components/pass-change/pass-change.component.ts
@@ -1,25 +1,23 @@
 import { Component } from '@angular/core';
-import { MenuItem } from 'primeng/api';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
 import { RouterModule } from '@angular/router';
 import { InputIconModule } from 'primeng/inputicon';
-import { IconFieldModule } from 'primeng/iconfield';
-import { InputTextModule } from 'primeng/inputtext';
-import { ButtonModule } from 'primeng/button';
-import { FormsModule } from '@angular/forms';
-import { MenubarModule } from 'primeng/menubar';
 import { CommonModule } from '@angular/common';
-import { DialogModule } from 'primeng/dialog';
 import { FooterComponent } from '../footer/footer.component';
-
 
 
 @Component({
   selector: 'app-pass-change',
   standalone: true,
-  imports: [RouterModule, InputIconModule, IconFieldModule, InputTextModule, ButtonModule, FormsModule, MenubarModule, CommonModule, DialogModule, FooterComponent],
+  imports: [RouterModule, InputIconModule,  InputTextModule, ButtonModule, FormsModule, CommonModule, FooterComponent, FloatLabelModule, PasswordModule],
   templateUrl: './pass-change.component.html',
   styleUrl: './pass-change.component.scss'
 })
 export class PassChangeComponent {
-
+  userValue: string | undefined;
+  passValue!: string;
 }
